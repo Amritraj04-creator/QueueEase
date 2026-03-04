f6e2a37ae31597b9d66b4904dd6d0e63fef2ce90
Amritraj04-creator <shriamritraj@gmail.com>
2026-03-05 01:57:27 +0530

    hover effect to link buttons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,35 @@
 function Navbar() {
   return (
-    <nav className="w-full shadow-md">
+    <nav className="w-full bg-blue-600 text-white shadow-md">
       <div className="flex justify-between items-center px-6 py-4">
         <h1 className="text-xl font-bold">
           QueueEase
         </h1>
 
-        <div className="flex gap-6">
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <a href="#">Login</a>
-          <a href="#">Sign Up</a>
-          <a href="#">Register Clinic</a>
-        </div>
+        <div className="flex gap-4 items-center">
+
+  <a href="#" className="px-4 py-2 rounded-md hover:bg-blue-500 hover:scale-105 hover:shadow-lg transform transition duration-200 cursor-pointer"
+>
+  Home
+</a>
+
+  <a href="#" className="px-4 py-2 rounded-md hover:bg-blue-500 hover:scale-105 hover:shadow-lg transform transition duration-200 cursor-pointer">
+    About
+  </a>
+
+  <a href="#" className="px-4 py-2 rounded-md hover:bg-blue-500 hover:scale-105 hover:shadow-lg transform transition duration-200 cursor-pointer">
+    Login
+  </a>
+
+  <a href="#" className="px-4 py-2 rounded-md hover:bg-blue-500 hover:scale-105 hover:shadow-lg transform transition duration-200 cursor-pointer">
+    Sign Up
+  </a>
+
+  <a href="#" className="px-4 py-2 bg-white text-blue-600 rounded-md hover:bg-gray-200 hover:scale-105 hover:shadow-lg transform transition duration-200 cursor-pointer">
+    Register Clinic
+  </a>
+
+</div>
       </div>
     </nav>
   )
